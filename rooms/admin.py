fbe7096370a32fdb568942fbee9d9a2342aa210a
gwuiinkim <gwuiinkim@gmail.com>
2021-09-02 13:40:24 +0000

    room admin

--- rooms/admin.py
+++ rooms/admin.py
@@ -11,4 +11,57 @@ class ItemAdmin(admin.ModelAdmin):
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
     
-    pass
+    """Room Admin Definition"""
+    
+    fieldsets = (
+        (
+            "Basic Info", 
+            {
+                "fields" : 
+                ("name", "description", "country", "address", "price",)
+            }
+        ),
+        (
+            "More About the Space",
+            {
+                "classes" : ("collapse"),
+                "fields" : 
+                ("amenities", "facilities", "house_rules",)
+            }
+        )
+    )
+    
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "check_out",
+        "instant_book",   
+        "count_amenities"
+    )
+    
+    #can access through foreignkey by using __
+    list_filter = (
+        "host__superhost",
+        "instant_book", 
+        "room_type",
+        "amenities",
+        "facilities",
+        "house_rules",
+        "city", 
+        "country",
+    )
+
+    search_fields = ("=city", "^host__username")
+    # this field is very useful for many to many field
+    filter_horizontal = ("amenities", "facilities", "house_rules")
+    
+    def count_amenities(self, obj):
+        #obj means current row
+        return obj.amenities.count()
