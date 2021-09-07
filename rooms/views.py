0117bc55cd764cbe4457a2a78490cc57a7f88e69
gwuiinkim <gwuiinkim@gmail.com>
2021-09-07 13:55:17 +0000

    Django paginator

--- rooms/views.py
+++ rooms/views.py
@@ -1,26 +1,34 @@
 from math import ceil
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from . import models
 
 def all_rooms(request):
     
     page = request.GET.get("page")
-    page = int(page or 1)
-
-    page_size = 10
-    limit = page_size*page
-    offset = limit - page_size
-    all_rooms = models.Room.objects.all()[offset:limit]
+#    query set is lazy. just make queryset and it will be called later. so here, it's fine to do this 
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 10)
+    rooms = paginator.get_page(page)
+    return render(request, "rooms/home.html", {"rooms":rooms})
     
-    page_count = ceil(models.Room.objects.count()/page_size)
+    # page = request.GET.get("page")
+    # page = int(page or 1)
+
+    # page_size = 10
+    # limit = page_size*page
+    # offset = limit - page_size
+    # all_rooms = models.Room.objects.all()[offset:limit]
+    
+    # page_count = ceil(models.Room.objects.count()/page_size)
                       
-    return render (
-        request,
-        "rooms/home.html",
-        {
-            "potato": all_rooms,
-            "page":page,
-            "page_count": page_count,
-            "page_range": range(1, page_count),
-        },
-    )
+    # return render (
+    #     request,
+    #     "rooms/home.html",
+    #     {
+    #         "potato": all_rooms,
+    #         "page":page,
+    #         "page_count": page_count,
+    #         "page_range": range(1, page_count),
+    #     },
+    # )
