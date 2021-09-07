4afe34adc9596076f21a34e5e0400f582344eb88
gwuiinkim <gwuiinkim@gmail.com>
2021-09-07 14:15:43 +0000

    class based view

--- rooms/views.py
+++ rooms/views.py
@@ -1,34 +1,16 @@
 from math import ceil
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.views.generic import ListView
 from . import models
 
-def all_rooms(request):
-    
-    page = request.GET.get("page")
-#    query set is lazy. just make queryset and it will be called later. so here, it's fine to do this 
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 10)
-    rooms = paginator.get_page(page)
-    return render(request, "rooms/home.html", {"rooms":rooms})
-    
-    # page = request.GET.get("page")
-    # page = int(page or 1)
 
-    # page_size = 10
-    # limit = page_size*page
-    # offset = limit - page_size
-    # all_rooms = models.Room.objects.all()[offset:limit]
+#reference-> ccbv.co.uk 
+class HomeView(ListView):
     
-    # page_count = ceil(models.Room.objects.count()/page_size)
-                      
-    # return render (
-    #     request,
-    #     "rooms/home.html",
-    #     {
-    #         "potato": all_rooms,
-    #         "page":page,
-    #         "page_count": page_count,
-    #         "page_range": range(1, page_count),
-    #     },
-    # )
+    
+    """ HomeView Definition """
+    
+    model = models.Room
+    paginate_by = 10
+    paginate_orphans = 5
+    ordering = "created"
+    context_object_name = "rooms"
