28d1cf725b8597109420278ac7c004a1d41653dc
gwuiinkim <gwuiinkim@gmail.com>
2021-09-09 10:37:22 +0000

    404 page and room detail cbv

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = "rooms"
 
 #                     argument
-urlpatterns = [path("<int:pk>", views.room_detail, name="detail")]
+# urlpatterns = [path("<int:pk>", views.room_detail, name="detail")]
+urlpatterns = [path("<int:pk>", views.RoomDetail.as_view(), name="detail")]
--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,7 @@
 from math import ceil
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.http import Http404
+from django.views.generic import ListView, DetailView
 from django.urls import reverse
 from django.shortcuts import render, redirect
 from . import models
@@ -19,9 +20,15 @@ class HomeView(ListView):
     context_object_name = "rooms"
     
 
+# class RoomDetail(DetailView):
+    
+#     """ RoomDetail Definition """
+    
+#     model = models.Room
 def room_detail(request, pk):
      try:
         room = models.Room.objects.get(pk=pk)
         return render(request, "rooms/detail.html", {"room": room})
     except models.Room.DoesNotExist:
-        return redirect(reverse("core:home"))
+        # return redirect(reverse("core:home"))
+        raise Http404()
